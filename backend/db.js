ab565730a59d51c5ee3096e1c6378c360bdc49e5
KimMinWoooo <als33396dn@gmail.com>
2024-08-01 17:44:56 +0900

    hello

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
-const mysql = require("mysql");
+const mysql = require('mysql');
 const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT
+  connectionLimit: 10,
+  host: 'mysql', // Docker Compose 서비스 이름으로 변경
+  user: 'root', // MYSQL_ROOT_PASSWORD와 일치해야 함
+  password: 'johnahn',
+  database: 'myapp',
+  port: 3306,
 });
 exports.pool = pool;
